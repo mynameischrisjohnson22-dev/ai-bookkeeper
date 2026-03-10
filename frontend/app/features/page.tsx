b774744582df5f433f966d05a3bce2a165aa43dd
Christian <mynameischrisjohnson22@gmail.com>
2026-03-10 11:57:03 -0400

    add routes

---
+++ frontend/app/features/page.tsx
@@ -0,0 +1,9 @@
+export default function Features() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Albdy Features
+      </h1>
+    </div>
+  )
+}
